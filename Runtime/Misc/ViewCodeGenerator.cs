ebe857f20e6b2360bb537e3bdb7873f15e94fa74
moonzy <twenty2hmz@qq.com>
2022-03-05 16:55:02 +0800

    - 视图层关键代码自动生成

---
+++ Runtime/Misc/ViewCodeGenerator.cs
@@ -0,0 +1,63 @@
+﻿#if UNITY_EDITOR
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace MzFrame
+{
+    public static class ViewCodeGenerator
+    {
+
+        private const string HEAD = 
+            @"// 
+using System.Collections.Generic;
+using System;
+using MzFrame;
+using UnityEngine;
+using UnityEngine.UI;
+";
+        
+        public static void WriteHead(StreamWriter sw)
+        {
+            sw.Write(HEAD);
+        }
+
+        public static void GenerateViewCode(StreamWriter sw, string className, Dictionary<string, string> paramPath)
+        {
+            WriteHead(sw);
+            sw.Write($"public  partial class {className} : ViewInfo");
+            sw.Write("{");
+            foreach (var element in paramPath)
+            {
+                sw.WriteLine($"public ViewCompFinder {element.Key};");
+            }
+            // 构造方法
+            sw.Write($"public {className}(GameObject viewObject, string name) : base(viewObject, name)");
+            sw.Write("{");
+            foreach (var element in paramPath)
+            {
+                sw.WriteLine($"{element.Key} = new ViewCompFinder(viewObject.transform.Find(\"{element.Value}\"));");
+            }
+                
+            sw.Write("}");
+            sw.Write("}");
+        }
+
+        public static void GenerateViewLogic(StreamWriter sw, string className)
+        {
+            WriteHead(sw);
+            sw.Write($"public  partial class {className}");
+            sw.Write("{");
+            
+            sw.Write("        public override void OnOpened(params object[] args)");
+            sw.Write("        {");
+            
+            sw.Write("        }");
+            sw.Write("}");
+        }
+
+        
+    }
+}
+
+#endif
---
+++ Runtime/UI/ViewCompFinder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+namespace MzFrame
+{
+    /// <summary>
+    /// 这是用以查找组件的结构体。
+    /// </summary>
+    public struct ViewCompFinder
+    {
+        private Dictionary<Type, object> _cacheOfConponents;
+
+        public readonly Transform transform;
+
+        public ViewCompFinder(Transform transform)
+        {
+            this.transform = transform;
+            _cacheOfConponents = new Dictionary<Type, object>();
+        }
+        
+        public T Comp<T>() where T : Component
+        {
+            var t = typeof(T);
+            if (!_cacheOfConponents.TryGetValue(t, out object comp))
+            {
+                comp = transform.GetComponent<T>();
+                if (comp != null)
+                {
+                    _cacheOfConponents.Add(t, comp);
+                }
+            }
+
+            return (T)comp;
+        }
+    }
+}
