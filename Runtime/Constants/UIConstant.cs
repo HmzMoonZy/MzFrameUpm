a83f38a48c1d43691a7e748d510d06a4432d046e
moonzy <twenty2hmz@qq.com>
2022-03-05 12:49:43 +0800

    - 导入 Odin-Inspector - 快速创建 View 的编辑器扩展

---
+++ Runtime/Constants/UIConstant.cs
@@ -0,0 +1,30 @@
+﻿namespace MzFrame
+{
+    // UI Constant.
+    public partial class Constant
+    {
+        /// <summary>
+        /// MzFrame 通过 ESortLayer 枚举来实现层级管理。
+        /// 修改它后请重新初始化。
+        /// </summary>
+        public enum ViewSort
+        {
+            Lobby = 0,
+
+            Operation = 10,
+
+            SubSystem = 15,
+
+            Load = 20,
+
+            Tip = 30,
+
+            Warning = 40,
+
+            Guide = 50,
+
+            Debug = 100,
+        }
+        
+    }
+}
---
+++ Runtime/UI/ViewConfig.cs
@@ -0,0 +1,32 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace MzFrame
+{
+    public class ViewConfig : MonoBehaviour
+    {
+        [Title("层级")]
+        [EnumPaging]
+        public Constant.ViewSort Layer;
+
+        [Title("自动遮罩")]
+        public bool EnableAutoMask = true;
+        [ShowIf("EnableAutoMask")]
+        public bool ClickMaskTriggerClose = false;
+
+        [Title("动画")]
+        public bool EnableAutoAnimation = false;
+
+        [Title("是否缓存")]
+        public bool IsCache = true;
+
+
+        #if UNITY_EDITOR
+        [Button(ButtonSizes.Large)]
+        private void 编写逻辑层()
+        {
+            Debug.Log("CCCC!");
+        }
+        #endif
+    }
+}
