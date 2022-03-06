85acec48379bac6ae62d34d65f971dde12b0ecea
moonzy <twenty2hmz@qq.com>
2022-03-06 17:00:23 +0800

    - 修改项目结构

---
+++ Assets/Demo/MzFrameScripts/ViewTestInfo.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public partial class ViewTestInfo
+{
+    public override void OnOpened(params object[] args)
+    {
+        Debug.Log($"{ViewName} : OnOpened!");
+    }
+
+    private float time = 0;
+    public override void Update(float deltaTime)
+    {
+        time += deltaTime;
+        if (time >= 1)
+        {
+            time = 0;
+            Debug.Log("一秒过去了...");
+        }
+    }
+
+    private void __OnClickBtn_Button()
+    {
+        Debug.Log("Click!");
+    }
+}
---
+++ Assets/Demo/MzFrameScripts/__ViewWhiteInfo__.cs
@@ -0,0 +1,7 @@
+// 
+using System.Collections.Generic;
+using System;
+using MzFrame;
+using UnityEngine;
+using UnityEngine.UI;
+public  partial class ViewWhiteInfo : ViewInfo{public ViewWhiteInfo(GameObject viewObject, string name) : base(viewObject, name){}}
