4ee142e1fe8d8fe109b72cf79bc941af0cef4db9
moonzy <twenty2hmz@qq.com>
2022-03-07 13:50:14 +0800

    - 修复自动遮罩丢失问题 - 使用 Addressable 同步加载 UI 的方案

--- Runtime/Res/ResourcesManager.cs
+++ Runtime/Res/ResourcesManager.cs
@@ -1,17 +1,38 @@
-﻿namespace MzFrameUpm
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace MzFrameUpm
 {
     public struct ResourcesRequest<T>
     {
-        
+        public string assetName;
+        public List<string> labels;
+        public Action<T> onLoad;
     }
-    
+
+    /// <summary>
+    /// 基于 Addressable Assets System 的加载框架.
+    /// </summary>
     public static class ResourcesManager
     {
-        
-        
-        public static void Instantiate()
+        /// <summary>
+        /// 
+        /// </summary>
+        public static GameObject Instantiate(ResourcesRequest<GameObject> request, Transform parent = null,
+            bool instantiateInWorldSpace = false)
         {
-            
+            var handle = Addressables.InstantiateAsync(request.assetName, parent, instantiateInWorldSpace);
+            handle.WaitForCompletion();
+            if (handle.Status == AsyncOperationStatus.Succeeded && handle.IsDone)
+            {
+                return handle.Result;
+            }
+
+            Debug.LogError(handle.OperationException.Message);
+            return null;
         }
     }
 }
