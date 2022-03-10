914b5bd7bf2ddd8e7f6418d0667438c217220047
moonzy <twenty2hmz@qq.com>
2022-03-10 13:42:43 +0800

    - 提供可配置文件

--- Editor/GenerateViewPrefab.cs
+++ Editor/GenerateViewPrefab.cs
@@ -7,49 +7,53 @@ using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GenerateViewPrefab : Editor
+namespace MzFrame.Editor
 {
-    [MenuItem("GameObject/UI/Mz-DefaultCanvas")]
-    private static void GenerateDefaultViewCanvas()
+    public class GenerateViewPrefab : UnityEditor.Editor
     {
-        var view = new GameObject("ViewDefault", 
-            typeof(Canvas),
-            typeof(CanvasScaler), 
-            typeof(GraphicRaycaster), 
-            typeof(MzFrame.ViewConfig));
+        [MenuItem("GameObject/UI/Mz-DefaultCanvas")]
+        private static void GenerateDefaultViewCanvas()
+        {
+            var view = new GameObject("ViewDefault", 
+                typeof(Canvas),
+                typeof(CanvasScaler), 
+                typeof(GraphicRaycaster), 
+                typeof(MzFrame.ViewConfig));
             view.layer = 5;
 
-        var canvas = view.GetComponent<Canvas>();
-        canvas.renderMode = RenderMode.ScreenSpaceCamera;
-        canvas.worldCamera = FindObjectsOfType<Camera>().Where(camera => camera.transform.name.Equals("ViewCamera")).ToList()[0];
-        canvas.planeDistance = 100;
-        canvas.additionalShaderChannels = AdditionalCanvasShaderChannels.None;
-        canvas.pixelPerfect = true;
+            var canvas = view.GetComponent<Canvas>();
+            canvas.renderMode = RenderMode.ScreenSpaceCamera;
+            canvas.worldCamera = FindObjectsOfType<Camera>().Where(camera => camera.transform.name.Equals("ViewCamera")).ToList()[0];
+            canvas.planeDistance = 100;
+            canvas.additionalShaderChannels = AdditionalCanvasShaderChannels.None;
+            canvas.pixelPerfect = true;
 
-        var scaler = view.GetComponent<CanvasScaler>();
-        scaler.uiScaleMode = CanvasScaler.ScaleMode.ConstantPixelSize;
+            var scaler = view.GetComponent<CanvasScaler>();
+            scaler.uiScaleMode = CanvasScaler.ScaleMode.ConstantPixelSize;
         
         
-        var raycaster = view.GetComponent<GraphicRaycaster>();
-    }
+            var raycaster = view.GetComponent<GraphicRaycaster>();
+        }
     
-    [MenuItem("GameObject/UI/Mz-StaticCanvas")]
-    private static void GenerateStaticViewCanvas()
-    {
-        var view = new GameObject("ViewStatic", 
-            typeof(Canvas),
-            typeof(CanvasScaler),
-            typeof(MzFrame.ViewConfig));
-        view.layer = 5; //UI
+        [MenuItem("GameObject/UI/Mz-StaticCanvas")]
+        private static void GenerateStaticViewCanvas()
+        {
+            var view = new GameObject("ViewStatic", 
+                typeof(Canvas),
+                typeof(CanvasScaler),
+                typeof(MzFrame.ViewConfig));
+            view.layer = 5; //UI
         
-        var canvas = view.GetComponent<Canvas>();
-        canvas.renderMode = RenderMode.ScreenSpaceCamera;
-        canvas.worldCamera = FindObjectsOfType<Camera>().Where(camera => camera.transform.name.Equals("ViewCamera")).ToList()[0];
-        canvas.planeDistance = 100;
-        canvas.additionalShaderChannels = AdditionalCanvasShaderChannels.None;
-        canvas.pixelPerfect = true;
+            var canvas = view.GetComponent<Canvas>();
+            canvas.renderMode = RenderMode.ScreenSpaceCamera;
+            canvas.worldCamera = FindObjectsOfType<Camera>().Where(camera => camera.transform.name.Equals("ViewCamera")).ToList()[0];
+            canvas.planeDistance = 100;
+            canvas.additionalShaderChannels = AdditionalCanvasShaderChannels.None;
+            canvas.pixelPerfect = true;
 
-        var scaler = view.GetComponent<CanvasScaler>();
-        scaler.uiScaleMode = CanvasScaler.ScaleMode.ConstantPixelSize;
+            var scaler = view.GetComponent<CanvasScaler>();
+            scaler.uiScaleMode = CanvasScaler.ScaleMode.ConstantPixelSize;
+        }
     }
+
 }
